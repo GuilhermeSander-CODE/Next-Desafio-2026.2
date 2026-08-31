700e5d11bfb9642f04669461973dde9001e6a73c
GuilhermeSander-CODE <guilherme.sander@codejr.com.br>
2026-08-31 19:03:25 -0300

    style: ajusta estilização do modal de visualizar

--- components/modais-adm/visualizar.tsx
+++ components/modais-adm/visualizar.tsx
@@ -32,7 +32,7 @@ export default function ModalVisualizar({ produto, isOpen, onClose }: ModalVisua
 
     return createPortal(
         <div className="fixed inset-0 z-999 bg-black/60 backdrop-blur-sm flex items-center justify-center p-4">
-            <div className="relative  w-full max-w-md bg-titanium-white rounded-3xl p-6 shadow-2xl border border-gray-300 animate-in fade-in zoom-in-95 duration-200 max-h-[90vh] overflow-y-auto scrollbar-thumb-electric-violet">
+            <div className="relative w-full max-w-md bg-titanium-white rounded-3xl p-6 shadow-2xl border border-gray-300 animate-in fade-in zoom-in-95 duration-200 max-h-[90vh] overflow-y-auto scrollbar-thumb-electric-violet">
                 <button 
                     onClick={onClose}
                     className="absolute top-4 right-4 text-gray-700 hover:text-black transition-colors cursor-pointer"
@@ -53,16 +53,16 @@ export default function ModalVisualizar({ produto, isOpen, onClose }: ModalVisua
                         </div>
                     </div>
 
-                    <div className="space-y-3 text-left">
+                    <div className="space-y-3 w-full flex flex-col items-start justify-between">
 
-                        <div>
+                        <div className="flex flex-col items-start justify-between">
                             <label className="text-xs font-bold text-black  block mb-1">Titulo</label>
                             <h2 className="text-base text-gray-600 leading-snug">
                                 {produto.titulo}
                             </h2>
                         </div>
 
-                        <div>
+                        <div className="flex flex-col items-start justify-between">
                             <label className="text-xs font-bold text-black  block mb-1">Autor</label>
                             <p className="text-xs text-gray-600 leading-relaxed max-h-28 overflow-y-auto pr-1">
                                 {produto.autor}
@@ -70,28 +70,28 @@ export default function ModalVisualizar({ produto, isOpen, onClose }: ModalVisua
                         </div>
 
 
-                        <div>
+                        <div className="flex flex-col items-start justify-between">
                             <label className="text-xs font-bold text-black  block mb-1">Ano de Lançamento</label>
                             <p className="text-xs text-gray-600 leading-relaxed max-h-28 overflow-y-auto pr-1">
                                 {produto.ano}
                             </p>
                         </div>
                         
-                        <div>
+                        <div className="flex flex-col items-start justify-between">
                             <label className="text-xs font-bold text-black  block mb-1">Volume</label>
                             <p className="text-xs text-gray-600 leading-relaxed max-h-28 overflow-y-auto pr-1">
                                 {produto.volume}
                             </p>
                         </div>
 
-                        <div>
+                        <div className="flex flex-col items-start justify-between">
                             <label className="text-xs font-bold text-black  block mb-1">Sinopse</label>
                             <p className="text-xs text-gray-600 leading-relaxed max-h-28 overflow-y-auto scrollbar-thumb-electric-violet pr-1">
                                 {produto.descricao || "Sem descrição disponível."}
                             </p>
                         </div>
 
-                        <div className="pt-2  text-sm text-gray-600">
+                        <div className="pt-2  text-sm text-gray-600 flex flex-col items-start justify-between">
                             <label className="text-xs font-bold text-black block mb-1">Preço</label>
                             R$ {Number(produto.preco).toFixed(2).replace(".", ",")}
                         </div>
